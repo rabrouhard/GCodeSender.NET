12d5c62830f658bc4d0d772c7cba6c6163a7583f
martin2250 <martin.225@web.de>
2015-09-25 14:33:05 +0200

    Moved code behind main menu to separate file

---
+++ GCodeSender.NET/MainWindow/MainWindow.Menu.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace GCodeSender.NET
+{
+	partial class MainWindow
+	{
+		private void Menu_ListSerialports(object sender, RoutedEventArgs e)
+		{
+			menuItemConnect.Items.Clear();
+
+			foreach (string port in SerialPort.GetPortNames())
+			{
+				MenuItem portItem = new MenuItem();
+
+				portItem.Header = port;
+
+				portItem.Click += PortItem_Click;
+
+				menuItemConnect.Items.Add(portItem);
+			}
+
+			menuItemConnect.Items.Add(menuItemNetwork);
+		}
+
+		private void PortItem_Click(object sender, RoutedEventArgs e)
+		{
+			if (!Connection.ConnectSerial((string)((MenuItem)sender).Header))
+			{
+				App.Message("Serial Error");
+			}
+		}
+
+		private void MenuTextBoxLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+		{
+			if (menuMain.IsKeyboardFocusWithin)
+				e.Handled = true;
+		}
+		private void Menu_Connect_Network_Click(object sender, RoutedEventArgs e)
+		{
+			if (!Connection.ConnectNetwork(textBoxNetworkAddress.Text))
+			{
+				App.Message("Network Error");
+			}
+		}
+
+		private void menuItemDisconnect_Click(object sender, RoutedEventArgs e)
+		{
+			Connection.Disconnect();
+		}
+
+		private void textBoxNetworkAddress_Load(object sender, RoutedEventArgs e)
+		{
+			textBoxNetworkAddress.Text = Properties.Settings.Default.NetworkAddress;
+		}
+
+		private void textBoxNetworkAddress_Unload(object sender, RoutedEventArgs e)
+		{
+			Properties.Settings.Default.NetworkAddress = textBoxNetworkAddress.Text;
+		}
+
+	}
+}
--- GCodeSender.NET/MainWindow/MainWindow.xaml.cs
+++ GCodeSender.NET/MainWindow/MainWindow.xaml.cs
@@ -56,38 +56,6 @@ namespace GCodeSender.NET
 			menuItemDisconnect.Visibility = Visibility.Visible;
 		}
 
-		private void Menu_ListSerialports(object sender, RoutedEventArgs e)
-		{
-			menuItemConnect.Items.Clear();
-
-			foreach(string port in SerialPort.GetPortNames())
-			{
-				MenuItem portItem = new MenuItem();
-
-				portItem.Header = port;
-
-				portItem.Click += PortItem_Click;
-				
-				menuItemConnect.Items.Add(portItem);
-			}
-
-			menuItemConnect.Items.Add(menuItemNetwork);			
-		}
-
-		private void PortItem_Click(object sender, RoutedEventArgs e)
-		{
-			if(!Connection.ConnectSerial((string)((MenuItem)sender).Header))
-			{
-				App.Message("Serial Error");
-			}
-		}
-
-		private void MenuTextBoxLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
-		{
-			if (menuMain.IsKeyboardFocusWithin)
-				e.Handled = true;
-		}
-
 		private void Command_Open_Executed(object sender, ExecutedRoutedEventArgs e)
 		{
 			
@@ -97,29 +65,6 @@ namespace GCodeSender.NET
 		{
 
 		}
-
-		private void Menu_Connect_Network_Click(object sender, RoutedEventArgs e)
-		{
-			if(!Connection.ConnectNetwork(textBoxNetworkAddress.Text))
-			{
-				App.Message("Network Error");
-			}
-		}
-
-		private void menuItemDisconnect_Click(object sender, RoutedEventArgs e)
-		{
-			Connection.Disconnect();
-		}
-
-		private void textBoxNetworkAddress_Load(object sender, RoutedEventArgs e)
-		{
-			textBoxNetworkAddress.Text = Properties.Settings.Default.NetworkAddress;
-		}
-
-		private void textBoxNetworkAddress_Unload(object sender, RoutedEventArgs e)
-		{
-			Properties.Settings.Default.NetworkAddress = textBoxNetworkAddress.Text;
-        }
 
 		private void MainWindow_Closed(object sender, EventArgs e)
 		{
